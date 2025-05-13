89787dfee1909b78d8e3fb1e6103f060faa3873f
muhammadnabil17 <alian2007nabil@gmail.com>
2025-05-13 21:40:02 +0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,12 @@
 
-import picture from "./picture.png";
-
-
+import picture1 from "./Foto1.jpg";
+import picture2 from "./foto2.jpg";
+import picture3 from "./foto3.jpg";
+import picture4 from "./foto4.jpg";
+import picture5 from "./foto5.jpg";
+import picture6 from "./foto6.jpg";
+import picture7 from "./foto7.jpg";
+import picture8 from "./foto8.jpg";
 
 import fireworks from "./fireworks.gif";
 import christmas from "./christmas.gif";
@@ -11,7 +16,14 @@ import christmas from "./christmas.gif";
 
 export {
 
-picture,
+picture1,
+picture2,
+picture3,
+picture4,
+picture5,
+picture6,
+picture7,
+picture8,
 christmas,
 fireworks,
 };
